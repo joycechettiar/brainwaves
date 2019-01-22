0156bcab49f3f40ba01ffd7a13ea5c59c805c301
pravin-pandey <pravin.pandey.dev@gmail.com>
2019-01-22 13:18:38 +0530

    Update arduinofft_test.ino.ino

--- arduinofft_test.ino.ino
+++ arduinofft_test.ino.ino
@@ -1,7 +1,7 @@
 #include "arduinoFFT.h"
  
 #define SAMPLES 128             //Must be a power of 2
-#define SAMPLING_FREQUENCY 1000 //Hz, must be less than 10000 due to ADC
+#define SAMPLING_FREQUENCY 20 //Hz, must be less than 10000 due to ADC
  
 arduinoFFT FFT = arduinoFFT();
  
@@ -14,7 +14,7 @@ double vImag[SAMPLES];
 void setup() {
     Serial.begin(115200);
  
-    sampling_period_us = round(1000000*(1.0/SAMPLING_FREQUENCY));
+    sampling_period_us = round(100000*(1.0/SAMPLING_FREQUENCY));
 }
  
 void loop() {
@@ -24,8 +24,8 @@ void loop() {
     {
         microseconds = micros();    //Overflows after around 70 minutes!
      
-        vReal[i] = analogRead(0);
-        vImag[i] = 0;
+        vReal[i] = analogRead(A0);
+        vImag[i] = analogRead(A0);
      
         while(micros() < (microseconds + sampling_period_us)){
         }
@@ -38,17 +38,18 @@ void loop() {
     double peak = FFT.MajorPeak(vReal, SAMPLES, SAMPLING_FREQUENCY);
  
     /*PRINT RESULTS*/
-    //Serial.println(peak);     //Print out what frequency is the most dominant.
+    Serial.println(peak); //Print out what frequency is the most dominant.
+    
  
     for(int i=0; i<(SAMPLES/2); i++)
     {
         /*View all these three lines in serial terminal to see which frequencies has which amplitudes*/
-         
-        //Serial.print((i * 1.0 * SAMPLING_FREQUENCY) / SAMPLES, 1);
-        //Serial.print(" ");
+        Serial.print("value1:");
+        Serial.print((i * 1.0 * SAMPLING_FREQUENCY) / SAMPLES, 1);
+        Serial.println(" ");
         Serial.println(vReal[i], 1);    //View only this line in serial plotter to visualize the bins
     }
  
-    //delay(1000);  //Repeat the process every second OR:
-    while(1);       //Run code once
+    delay(5000);  //Repeat the process every second OR:
+    //while(1);       //Run code once
 }
